1e8362ab540ba4da6415c6ea19508ea946bec2e8
ismailrowaihy <ismailrowaihy@gmial.com>
2025-04-24 17:33:43 +0000

    added scirpt to print the size of some types

---
+++ hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main -entry point
+ * Return: Always 0
+ *it does the thing
+ */
+int main(void)
+{
+printf("Size of a char: %u byte(s)\n", sizeof(char));
+printf("Size of an int: %u byte(s)\n", sizeof(int));
+printf("Size of a long int: %u byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %u byte(s)\n", sizeof(long long int));
+printf("Size of a float: %u byte(s)\n", sizeof(float));
+return (0);
+}
