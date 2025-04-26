85c0ebd278a388f30a6863c7d89b96e45082920e
ismialrowaihy <ismialrowaihy@gmail.com>
2025-04-26 06:12:12 +0000

    added C script that print last digit and says some if its greater than 5 or less then 6 or is 0

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main -entry point
+ * Return: Always 0
+ *it does the thing
+ */
+int main(void)
+{
+	int n, last_digit;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	last_digit = n % 10;
+if (last_digit > 5)
+{
+printf("Last digit of %d is %d and is greater then 5\n", n, last_digit);
+}
+else if (last_digit < 6 && last_digit != 0)
+{
+printf("Last digit of %d is %d and is less then 6 and not 0\n", n, last_digit);
+}
+else if (last_digit < 6 && last_digit == 0)
+{
+printf("Last digit of %d is %d and is 0\n", n, last_digit);
+}
+return (0);
+}
