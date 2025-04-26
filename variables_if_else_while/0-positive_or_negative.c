2549c5e07c68df5be04a9df8be6b8c48fb47ae54
ismialrowaihy <ismialrowaihy@gmail.com>
2025-04-26 05:39:43 +0000

    added C script to print if num is positive or negative or zero/fixed typo/fixed betty thing

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,11 @@
 #include <stdio.h>
 /* more headers goes there */
 
-/* betty style doc for function main goes there */
+/**
+ * main -entry point
+ * Return: Always 0
+ *it does the thing betty
+ */
 int main(void)
 {
 	int n;
@@ -11,17 +15,17 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	if ( n > 0)
-	  {
-	    printf("%d is positive\n",n);
-	  }
-	else if(n < 0)
-	  {
-	    printf("%d is negative\n",n);
-	  }
-	else
-	  {
-	    printf("%d is zero\n",n);
-	  }
-	return (0);
+if (n > 0)
+{
+printf("%d is positive\n", n);
+}
+else if (n < 0)
+{
+printf("%d is negative\n", n);
+}
+else
+{
+printf("%d is zero\n", n);
+}
+return (0);
 }
