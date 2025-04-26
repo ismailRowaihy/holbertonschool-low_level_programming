28de4e0cf28f9f647038627aedaffa4d32942947
ismialrowaihy <ismialrowaihy@gmail.com>
2025-04-26 06:39:58 +0000

    added C script that prints all alphabet lowercase followed by new line

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main -entry point
+ * Return: Always 0
+ *it does the thing
+ */
+int main(void)
+{
+char i;
+for (i = 'a'; i <= 'z' ; ++i)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
