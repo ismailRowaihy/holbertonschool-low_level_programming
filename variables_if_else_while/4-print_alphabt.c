b1c0b338945434f9f80e606082eb1814a321b40c
ismialrowaihy <ismialrowaihy@gmail.com>
2025-04-26 06:54:15 +0000

    added script that print lower alphabet bit not q and e

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main -entry point
+ * Return: Always 0
+ *it does the thing
+ */
+int main(void)
+{
+char i;
+for (i = 'a'; i <= 'z' ; ++i)
+{
+if (i == 'q' || i == 'e')
+{
+continue;
+}
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
