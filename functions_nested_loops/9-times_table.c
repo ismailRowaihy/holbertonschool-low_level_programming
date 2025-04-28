d341ca9b342d6a5345c3da11852315bf136e5955
ismailrowaihy <ismailrowaihy@gmail.com>
2025-04-28 06:56:19 +0000

    added C program the prints 9 times table

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * times_table -entry point
+ * Return: Always 0
+ *it does the thing
+ */
+void times_table(void)
+{
+int i,j,k,l,h;
+
+for (i = 0; i <= 9; ++i)
+{
+for (j = 0; j <= 9; ++j)
+{
+k = i * j;
+l = k % 10;
+h = k / 10;
+if (k > 9)
+{
+_putchar(',');
+_putchar(' ');
+_putchar( '0'+h);
+_putchar('0'+l);
+}
+else
+{
+if (j != 0)
+{   
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+}
+_putchar('0'+k);
+}
+}
+_putchar('\n');
+}
+}
