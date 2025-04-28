0a740be232892cf981fce989dc695f83b0f5317c
ismailrowaihy <ismailrowaihy@gmail.com>
2025-04-28 05:14:00 +0000

    added C program that prints every minute of the day/fixed the _

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -11,12 +11,12 @@ int i = 0, j = 0, k = 0, l = 0;
 
 while (1)
 {
-putchar('0' + l);
-putchar('0' + k);
-putchar(':');
-putchar('0' + j);
-putchar('0' + i);
-putchar('\n');
+_putchar('0' + l);
+_putchar('0' + k);
+_putchar(':');
+_putchar('0' + j);
+_putchar('0' + i);
+_putchar('\n');
 if (l == 2 && k == 3 && j == 5 && i == 9)
 {
 break;
