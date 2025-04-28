a5ab8057cf74c18afe25d310fb8a471dd6ca4e43
ismailrowaihy <ismailrowaihy@gmail.com>
2025-04-28 07:09:07 +0000

    added C program that adds 2 ints

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * add -entry point
+ * @n1: numbers to add
+ * @n2: numbers to add
+ * Return: total
+ *it does the thing
+ */
+int add(int n1, int n2)
+{
+return (n1 + n2);
+}
