033da0e3f8cd60828f48e757c94fdd5bfc62f66b
ismailrowaihy <ismailrowaihy@gmail.com>
2025-05-05 07:33:16 +0000

    added C program that prints 0 to 14 ten times

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * more_numbers -entry point
+ * Return: Always 0
+ *it does the thing
+ */
+void more_numbers(void)
+{
+int i, j;
+char string[] = "01234567891011121314";
+for (i = 0; i <= 10; i++)
+{
+for (j = 0; string[j]; j++)
+{
+putchar(string[j]);
+}
+putchar('\n');
+}
+}
