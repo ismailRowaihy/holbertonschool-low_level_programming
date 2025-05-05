ebf0a29811596467a8be7fde17305e07b26a5316
ismailrowaihy <ismailrowaihy@gmail.com>
2025-05-05 06:25:38 +0000

    added C program that multiplies two ints

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * mul -entry point
+ * @a: is an int that will be multiplied
+ * @b: is an int that will be multiplied
+ * Return: Always 0
+ *it does the thing
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
