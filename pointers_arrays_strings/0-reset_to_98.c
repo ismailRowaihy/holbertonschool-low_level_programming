768dfb4debb0209941dfc8ea7fc81f67e155c6bf
ismailrowaihy <ismailrowaihy@gmail.com>
2025-05-08 06:20:47 +0000

    added C program that takes a pointer and updates the value it points to 98

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * reset_to_98 -entry point
+ * @n: an int pointer
+ * Return: Always 0
+ *it does the thing
+ */
+void reset_to_98(int *n)
+{
+  *n =98;
+}
