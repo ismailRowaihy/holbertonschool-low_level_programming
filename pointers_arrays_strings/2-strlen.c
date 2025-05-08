ed8349b3f3bb4c5b7e2be7074ad48dfc18660314
ismailrowaihy <ismailrowaihy@gmail.com>
2025-05-08 07:01:50 +0000

    added C progarm that returns the len of a string

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *_strlen -entry point
+ *@s: a char pointer
+ * Return: Always 0
+ *it does the thing
+ */
+int _strlen(char *s)
+{
+int i;
+for (i = 0; s[i]; i++)
+{
+}
+return (i);
+}
