1e41f04eeb10eb26bfd669d9856cc42088be9b0a
ismailrowaihy <ismailrowaihy@gmail.com>
2025-05-09 17:11:32 +0000

    added C program that prints the first char then every other char

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *puts2 -entry point
+ *@str: a char pointer
+ * Return: Always 0
+ *it does the thing
+ */
+void puts2(char *str)
+{
+int i;
+for (i = 0; str[i]; i += 2)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+}
