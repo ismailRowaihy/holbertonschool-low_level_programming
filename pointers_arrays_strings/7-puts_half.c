5080494844b0cbe2957007c3e0bd50062773ecca
ismailrowaihy <ismailrowaihy@gmail.com>
2025-05-09 17:43:51 +0000

    added C program that prints hlaf of a string

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *puts_half -entry point
+ *@s: a char pointer
+ * Return: Always 0
+ *it does the thing
+ */
+void puts_half(char *s)
+{
+int i, j;
+for (i = 0; s[i]; i++)
+{
+}
+if (i % 2 == 0)
+{
+i /= 2;
+}
+else
+{
+i =( i + 1) / 2;
+}
+for (j =i;s[j];j++)
+{
+_putchar(s[j]);
+}
+_putchar('\n');
+}
