7aa73e2234833961d9d5aa6a19d59e0e8d46c7b6
ismailrowaihy <ismailrowaihy@gmail.com>
2025-05-11 16:52:44 +0000

    added C program that appends a string to another/fixed betty

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -10,9 +10,10 @@
 char *_strcat(char *dest, char *src)
 {
 int i, j;
-for (i = 0; dest[i]; i++);
+for (i = 0; dest[i]; i++)
+;
 for (j = 0; src[j]; j++, i++)
 dest[i] = src[j];
-dest[i] ='\0';
+dest[i] = '\0';
 return (dest);
 }
