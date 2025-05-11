c19196134cfd44d5227d439572df20c6fb0a6684
ismailrowaihy <ismailrowaihy@gmail.com>
2025-05-11 16:49:21 +0000

    added C program that appends a string to another

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strcat -entry point
+ *@dest: a string to append to
+ *@src: sring to copy
+ * Return: Always 0
+ *it does the thing
+ */
+char *_strcat(char *dest, char *src)
+{
+int i, j;
+for (i = 0; dest[i]; i++);
+for (j = 0; src[j]; j++, i++)
+dest[i] = src[j];
+dest[i] ='\0';
+return (dest);
+}
