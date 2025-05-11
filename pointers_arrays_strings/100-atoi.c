0bf3497c304bc99990081a31fc7dc4475865e305
ismailrowaihy <ismailrowaihy@gmail.com>
2025-05-11 16:28:45 +0000

    added C program that return the number from a string /fixed betty

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -8,25 +8,23 @@
  */
 int _atoi(char *s)
 {
-int i, j=0, check = 1, fn = 0;
-for (i=0; s[i]; i++)
+int i, j = 0, check = 1, fn = 0;
+for (i = 0; s[i]; i++)
 {
 if (s[i] == '-')
 {
-if(fn == 0){
+if (fn == 0)
 check *= -1;
-}	
 }
 if (s[i] <= '9' && s[i] >= '0')
 {
 if (!fn)
-fn ++;		        			
+fn++;
 j *= 10;
-j += s[i]- '0';			
+j += s[i] - '0';
 }
-if (fn && s[i+1] > '9' && s[i+1] > '0'){
+if (fn && s[i + 1] > '9' && s[i + 1] > '0')
 break;
-}
 }
 j *= check;
 return (j);
