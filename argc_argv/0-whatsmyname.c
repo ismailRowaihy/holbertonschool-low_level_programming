47f4f2edd24fff7f7a8cb187e180254417107da9
ismailrowaihy <ismailrowaihy@gmail.com>
2025-05-17 15:22:03 +0000

    added c file that prints its name

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main -entry point
+ *@argc: arg counts
+ *@argv: arg array of passed words
+ * Return: Always 0
+ *it does the thing
+ */
+int main(int argc, char *argv[])
+{
+(void)argc;
+printf("%s\n",argv[0]);
+return(0);  
+}
