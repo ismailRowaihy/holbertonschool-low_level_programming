3afeac7c2efb8ed603ec2c073b8226049630eed5
ismailrowaihy <ismailrowaihy@gmail.com>
2025-05-17 07:21:17 +0000

    added c program that returnt the len of a string

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strlen_recursion -entry point
+ *@s: a string to count
+ * Return: Always 0
+ *it does the thing
+ */
+int _strlen_recursion(char *s)
+{
+if (*s == '\0')
+return (0);
+
+return ( 1 + _strlen_recursion(s + 1));
+}
