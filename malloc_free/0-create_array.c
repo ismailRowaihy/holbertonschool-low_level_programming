8d2ce1690ec8bd519dca8b689a5bc60ae8a0293f
ismailrowaihy <ismailrowaihy@gmail.com>
2025-05-19 06:32:13 +0000

    added C program that creates an array and appends with some char based on size/fixed betty and malloc return ?

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -4,29 +4,32 @@
 /**
  * create_array -entry point
  *@size: size of the array
+ *@c: the char to append
  * Return: Always 0
  *it does the thing
  */
 char *create_array(unsigned int size, char c)
 {
-  unsigned int i;
-  char *newAr;
+unsigned int i;
+char *newAr;
 
-  if (size == 0)
-    return (NULL);
+if (size == 0)
+return (NULL);
 
-      newAr =malloc(size+1);
-      
-      for (i = 0; i <= size; i++)
-	{
-	  if (i == size)
-	    {
-	      newAr[i] = '\0';
-	    }
-	  else
-	    {
-	     newAr[i]= c;
-	    }
-	}
-      return (newAr);
+newAr = malloc(sizeof(char) * size + 1);
+if (newAr == NULL)
+return (NULL);
+
+for (i = 0; i <= size; i++)
+{
+if (i == size)
+{
+newAr[i] = '\0';
+}
+else
+{
+newAr[i] = c;
+}
+}
+return (newAr);
 }
