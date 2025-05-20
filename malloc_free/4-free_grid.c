96c60f0d16e40f27957bba036daedbe4deba62a6
ismailrowaihy <ismailrowaihy@gmail.com>
2025-05-20 14:48:06 +0000

    added c program the frees a grid

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * free_grid -entry point
+ *@grid: 2d array grid 
+ *@height: height of the grid
+ * Return: Always 0
+ *it does the thing
+ */
+void free_grid(int **grid,int height)
+{
+int i;
+
+for (i = 0; i < height; i++)
+free(grid[i]);
+free(grid);
+
+}
