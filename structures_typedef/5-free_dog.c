399e04a77a00ff71a56e20c02df03c25992913eb
ismailrowaihy <ismailrowaihy@gmail.com>
2025-05-22 08:57:56 +0000

    added C program to Free a dog

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - check the code
+ *@d: a dog to free
+ * Return: Always 0.
+ */
+void free_dog(dog_t *d)
+{
+if (d == NULL)
+return;
+free(d->name);
+free(d->owner);
+free(d);
+}
