82ae3751e41c1393eec0f9287b410458a7f7cbbb
ismailrowaihy <ismailrowaihy@gmail.com>
2025-05-22 06:28:16 +0000

    added dog struct file /fixed betty

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,11 +1,18 @@
 #ifndef DOG_H
 #define DOG_H
-
+/**
+ *struct dog - struct for dog
+ *@name: name of the dog
+ *@age: age of the dog
+ *@owner: owner of the dog
+ */
 struct dog
 {
-  char *name;
-  float age;
-  char *owner;
+
+char *name;
+float age;
+char *owner;
+
 };
 
 #endif
