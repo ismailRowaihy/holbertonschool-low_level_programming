b79d9280a7057d96de1df0358d77a5a9b990a862
ismailrowaihy <ismailrowaihy@gmail.com>
2025-05-22 08:44:16 +0000

    added c program that creates a new dog/fixed betty

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -9,43 +9,43 @@
  *@owner: owner of new dog
  * Return: Always 0.
  */
-dog_t *new_dog(char *name, float age,char *owner)
+dog_t *new_dog(char *name, float age, char *owner)
 {
- int i, nL, oL;
+int i, nL, oL;
 dog_t *newDog;
 
- newDog = malloc(sizeof(dog_t));
-  if (newDog == NULL)
-    return (NULL);
+newDog = malloc(sizeof(dog_t));
+if (newDog == NULL)
+return (NULL);
 
-  for(nL = 0; name[nL];nL++)
-    ;
-  for(oL = 0; owner[oL];oL++)
-    ;
+for (nL = 0; name[nL]; nL++)
+;
+for (oL = 0; owner[oL]; oL++)
+;
 
 
-  newDog->name = malloc(nL + 1);
-  if (newDog->name == NULL)
-  {
-    free(newDog);
-    return (NULL);
-  }
-  newDog->owner = malloc(oL + 1);
-  if (newDog->owner == NULL)
-    {
+newDog->name = malloc(nL + 1);
+if (newDog->name == NULL)
+{
+free(newDog);
+return (NULL);
+}
+newDog->owner = malloc(oL + 1);
+if (newDog->owner == NULL)
+{
 free(newDog->name);
 free(newDog);
 return (NULL);
-    }
-  for(i = 0;name[i];i++)
-    newDog->name[i] = name[i];
+}
+for (i = 0; name[i]; i++)
+newDog->name[i] = name[i];
 
-  newDog->name[i] = name[i];
+newDog->name[i] = name[i];
 
-  for(i = 0;owner[i];i++)
-    newDog->owner[i] = owner[i];
+for (i = 0; owner[i]; i++)
+newDog->owner[i] = owner[i];
 
-  newDog->owner[i] = owner[i];
-  newDog->age = age;
-  return (newDog);
+newDog->owner[i] = owner[i];
+newDog->age = age;
+return (newDog);
 }
