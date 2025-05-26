967812c8cb2b8844fea62f9fcb719c79ab814cec
ismailrowaihy <ismailrowaihy@gmail.com>
2025-05-26 07:07:28 +0000

    added c program that returns the index if found

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * int_index - entry point
+ *@array: array to iterat on
+ *@size: size of the array
+ *@cmp: pointer to function
+ *Return: the index if found , -1 if no match
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i;
+
+if (size <= 0)
+return (-1);
+
+for (i = 0; i < size; i++)
+{
+if (cmp(array[i]) != 0)
+return (i);
+}
+
+return (-1);
+}
