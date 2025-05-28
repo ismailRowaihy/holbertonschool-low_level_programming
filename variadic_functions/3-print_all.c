8287588cd0b62cd76836cedbf50813825d405ca4
ismailrowaihy <ismailrowaihy@gmail.com>
2025-05-28 06:30:29 +0000

    swtich case test

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -21,33 +21,31 @@ va_start(TheList, format);
 while (format[i] != '\0')
 {
 
-if (format[i] == 'c')
-{
-holderC = va_arg(TheList, int);
-printf("%c", holderC);
-}
-else if (format[i] == 'i')
-{
-holderI = va_arg(TheList, int);
-printf("%d", holderI);
-}
-else if (format[i] == 'f')
-{
-holderF = va_arg(TheList, double);
-printf("%f", holderF);
-}
-else if (format[i] == 's' )
-{
-holderS = va_arg(TheList, char*);
-if (holderS == NULL)
-{
-printf("(nil)");
-}
-else
-{
-printf("%s", holderS);
-}}
-
+  switch (format[i])
+    {
+    case 'c':
+      holderC = va_arg(TheList, int);
+      printf("%c", holderC);
+      break;
+    case 'i':
+      holderI = va_arg(TheList, int);
+      printf("%d", holderI);
+      break;
+    case 'f':
+      holderF = va_arg(TheList, double);
+      printf("%f",holderF);
+      break;
+    case 's':
+      holderS = va_arg(TheList, char *);
+      if (holderS == NULL)
+	{
+	printf("(nil)");
+	break;
+	}
+      printf("%s",holderS);
+      break;
+    }
+  
 i++;
 }
 printf("\n");
