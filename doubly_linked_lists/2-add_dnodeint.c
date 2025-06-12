0e5ce94d2f43f000f3ae7d44cb5c50ef1d2d1577
ismailrowaihy <ismailrowaihy@gmail.com>
2025-06-12 10:35:09 +0000

    added c program that adds a node at the beginning of a doubly linked list/fixed betty

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -4,34 +4,35 @@
 #include "lists.h"
 
 /**
- *add_dlistint - adds a node  at the beginning
- *@h: head of the doubly linked list
+ *add_dnodeint - adds a node  at the beginning
+ *@head: head of the doubly linked list
+ *@n: data to add to the node
  *
  *Return: new node at the beginnig
  */
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-  dlistint_t *node;
+dlistint_t *node;
 
-  node = malloc(sizeof(dlistint_t));
-  if (node == NULL)
-    return (NULL);
+node = malloc(sizeof(dlistint_t));
+if (node == NULL)
+return (NULL);
 
-  node->n = n;
-  node->prev = NULL;
+node->n = n;
+node->prev = NULL;
 
-  if (*head == NULL)
-    {
-      node->next = NULL;
-      *head = node;
-    }
-  else
-    {
-      node->next = *head;
-      (**head).prev = node;
-      *head = node;
-    }
+if (*head == NULL)
+{
+node->next = NULL;
+*head = node;
+}
+else
+{
+node->next = *head;
+(**head).prev = node;
+*head = node;
+}
 
-  return (node);
+return (node);
 }
