7c361137d1a33c89bdfd94f494166c2f5ca3b9c9
ismailrowaihy <ismailrowaihy@gmail.com>
2025-06-15 07:12:39 +0000

    added C program that prints a text in a text file

---
+++ file-io/0-read_textfile.c
@@ -0,0 +1,40 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "main.h"
+
+/**
+ *read_textfile- reads a file and prints it
+ *@filename: name of the file to print
+ *@letters: how many letters to print
+ *
+ *Return: the number of printed letters
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+
+int fd, readfile, count;
+char *buffer;
+
+if (filename == NULL)
+return (0);
+
+fd = open(filename, O_RDONLY);
+if (fd == -1)
+return (0);
+
+buffer = malloc(letters);
+if (buffer == NULL)
+return (0);
+
+readfile = read(fd, buffer, letters);
+
+count = write(1, buffer, readfile);
+if (count == -1)
+return (0);
+
+close(fd);
+return (count);
+}
