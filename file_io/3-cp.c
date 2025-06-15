f3b4d42b02d3b88e53728d7bd64b91afa2daf821
ismailrowaihy <ismailrowaihy@gmail.com>
2025-06-15 19:10:20 +0000

    Added C program that copy a file

---
+++ file_io/3-cp.c
@@ -0,0 +1,75 @@
+#include <string.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "main.h"
+
+/**
+ *main- copies content of a file to another file
+ *@argc: num of args
+ *@argv: list of sting args
+ *
+ *Return: 0 on success or error code if failed
+ */
+
+int main(int argc, char *argv[])
+{
+int file_from, file_to, Rbytes, Wbytes;
+char buffer[1024];
+
+if (argc != 3)
+{
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+exit(97);
+}
+
+file_from = open(argv[1], O_RDONLY);
+if (file_from == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+exit(98);
+}
+
+file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+if (file_to == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+close(file_from);
+exit(99);
+}
+
+while ((Rbytes = read(file_from, buffer, 1024)) > 0)
+{
+Wbytes = write(file_to, buffer, Rbytes);
+if (Wbytes != Rbytes)
+{
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+close(file_from);
+close(file_to);
+exit(99);
+}
+}
+
+if (Rbytes == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+close(file_from);
+close(file_to);
+exit(98);
+}
+
+if (close(file_from) == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+exit(100);
+}
+
+if (close(file_to) == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
+exit(100);
+}
+
+return (0);
+}
